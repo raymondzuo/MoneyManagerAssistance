d3159eccd6911eed2450ce661672105ce1f7db7e
raymond <raymond.zuo@live.com>
2014-09-08 10:22:01 +0800

    修改首页样式，添加hub

--- MoneyManagerAssistance/MainPage.xaml.cs
+++ MoneyManagerAssistance/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,6 +28,8 @@ namespace MoneyManagerAssistance
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+            StatusBar.GetForCurrentView().BackgroundOpacity = 0;
+            
         }
 
         /// <summary>
