e776f866b63c833f7b968c41a8917fa595a795e1
raymond <raymond.zuo@live.com>
2014-09-09 00:01:17 +0800

    添加首页视图模式选择，底部菜单栏（底部菜单栏显示目前有问题）

--- MoneyManagerAssistance/MainPage.xaml.cs
+++ MoneyManagerAssistance/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -23,12 +24,21 @@ namespace MoneyManagerAssistance
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private AppBarButton checkInButton;
+        private AppBarButton marktReviewButton;
+        private AppBarButton bbsSearchButton;
+        private AppBarButton jifenButton;
+        //private AppBarButton refreshButton;
+        private AppBarButton settingButton;
+        private AppBarButton loginOrOutButton;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
             StatusBar.GetForCurrentView().BackgroundOpacity = 0;
+            InitAppBar();
             
         }
 
@@ -46,6 +56,72 @@ namespace MoneyManagerAssistance
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
             // 如果使用由某些模板提供的 NavigationHelper，
             // 则系统会为您处理该事件。
+        }
+
+        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private void MainPageHub_OnSectionsInViewChanged(object sender, SectionsInViewChangedEventArgs e)
+        {
+            if (this.BottomAppBar == null)
+            {
+                return;
+            }
+
+            var bar = this.BottomAppBar as CommandBar;
+            bar.PrimaryCommands.Clear();
+
+
+            
+        }
+
+        private void InitAppBar()
+        {
+            CommandBar appBar = new CommandBar();
+
+            checkInButton = new AppBarButton() { Label = "添加账务" };
+            checkInButton.Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBarIcon/Add-New.png", UriKind.Absolute) };
+            appBar.PrimaryCommands.Add(checkInButton);
+
+            marktReviewButton = new AppBarButton() { Label = "好评" };
+            marktReviewButton.Icon = new SymbolIcon(Symbol.Like);
+
+            bbsSearchButton = new AppBarButton() { Label = "论坛搜索" };
+            bbsSearchButton.Icon = new SymbolIcon(Symbol.Zoom);
+
+            jifenButton = new AppBarButton() { Label = "积分" };
+            //jifenButton.Icon = new SymbolIcon(Symbol.Favorite);
+            jifenButton.Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBarIcon/Add-New.png", UriKind.RelativeOrAbsolute) };
+
+            //refreshButton = new AppBarButton() { Label = "刷新" };
+            //refreshButton.Icon = new SymbolIcon(Symbol.Refresh);
+            //refreshButton.Click += refreshButton_Click;
+
+            settingButton = new AppBarButton() { Label = "设置" };
+            settingButton.Icon = new SymbolIcon(Symbol.Setting);
+
+            loginOrOutButton = new AppBarButton();
+            //loginOrOutButton.Icon = new SymbolIcon(Symbol.Setting);
+
+            var refreshMenu = new AppBarButton() { Label = "刷新" };
+            appBar.SecondaryCommands.Add(refreshMenu);
+
+            appBar.Visibility = Visibility.Visible;
+            this.BottomAppBar = appBar;
+            SetAppBarBackgroundColor();
+        }
+
+        public void SetAppBarBackgroundColor(double Opacity = 0.88)
+        {
+            if (this.BottomAppBar != null)
+            {
+                this.BottomAppBar.Opacity = Opacity;
+                this.BottomAppBar.IsSticky = false;
+                this.BottomAppBar.Foreground = new SolidColorBrush(Colors.White);
+                
+            }
         }
     }
 }
