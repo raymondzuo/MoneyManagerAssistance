bc4e32f689599a321d6f90aa23b489121a04ec46
raymond <raymond.zuo@live.com>
2014-09-09 23:22:24 +0800

    添加汇率换算功能；首页添加底部菜单栏；

--- MoneyManagerAssistance/MainPage.xaml.cs
+++ MoneyManagerAssistance/MainPage.xaml.cs
@@ -24,7 +24,7 @@ namespace MoneyManagerAssistance
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private AppBarButton checkInButton;
+        private AppBarButton addNewAccountings;
         private AppBarButton marktReviewButton;
         private AppBarButton bbsSearchButton;
         private AppBarButton jifenButton;
@@ -70,8 +70,8 @@ namespace MoneyManagerAssistance
                 return;
             }
 
-            var bar = this.BottomAppBar as CommandBar;
-            bar.PrimaryCommands.Clear();
+            //var bar = this.BottomAppBar as CommandBar;
+            //bar.PrimaryCommands.Clear();
 
 
             
@@ -79,34 +79,13 @@ namespace MoneyManagerAssistance
 
         private void InitAppBar()
         {
-            CommandBar appBar = new CommandBar();
+            var appBar = new CommandBar();
 
             checkInButton = new AppBarButton() { Label = "添加账务" };
-            checkInButton.Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBarIcon/Add-New.png", UriKind.Absolute) };
+            checkInButton.Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBarIcon/CheckIn.png", UriKind.RelativeOrAbsolute) };
+
             appBar.PrimaryCommands.Add(checkInButton);
-
-            marktReviewButton = new AppBarButton() { Label = "好评" };
-            marktReviewButton.Icon = new SymbolIcon(Symbol.Like);
-
-            bbsSearchButton = new AppBarButton() { Label = "论坛搜索" };
-            bbsSearchButton.Icon = new SymbolIcon(Symbol.Zoom);
-
-            jifenButton = new AppBarButton() { Label = "积分" };
-            //jifenButton.Icon = new SymbolIcon(Symbol.Favorite);
-            jifenButton.Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBarIcon/Add-New.png", UriKind.RelativeOrAbsolute) };
-
-            //refreshButton = new AppBarButton() { Label = "刷新" };
-            //refreshButton.Icon = new SymbolIcon(Symbol.Refresh);
-            //refreshButton.Click += refreshButton_Click;
-
-            settingButton = new AppBarButton() { Label = "设置" };
-            settingButton.Icon = new SymbolIcon(Symbol.Setting);
-
-            loginOrOutButton = new AppBarButton();
-            //loginOrOutButton.Icon = new SymbolIcon(Symbol.Setting);
-
-            var refreshMenu = new AppBarButton() { Label = "刷新" };
-            appBar.SecondaryCommands.Add(refreshMenu);
+            
 
             appBar.Visibility = Visibility.Visible;
             this.BottomAppBar = appBar;
